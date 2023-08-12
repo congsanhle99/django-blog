57264c73a0e553a94e4f16d6aa0f680059f02298
CongSanhLe <congsanhle1999@gmail.com>
2023-08-13 06:38:05 +0700

    blog model

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -13,3 +14,27 @@ class Category(models.Model):
 
     def __str__(self):
         return self.category_name
+
+
+STATUS_CHOICES = (
+    (0, "Draft"),
+    (1, "Published"),
+    (2, "Private"),
+)
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=150, unique=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    featured_image = models.ImageField(upload_to='uploads/%Y/%m/%d')
+    short_description = models.TextField(max_length=500)
+    blog_body = models.TextField(max_length=2000)
+    status = models.IntegerField(choices=STATUS_CHOICES, default=0)
+    is_featured = models.BooleanField(default=False)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
