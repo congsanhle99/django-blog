67038a534e62cf4515122c4d7b518cc8d077739d
CongSanhLe <congsanhle1999@gmail.com>
2023-08-13 06:42:29 +0700

    generate slug base on title

--- blogs/admin.py
+++ blogs/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from .models import Blog, Category
 
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Blog)
+admin.site.register(Blog, BlogAdmin)
