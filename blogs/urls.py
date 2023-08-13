64f7854c8261125f4537b664550745c34d8ed6af
CongSanhLe <congsanhle1999@gmail.com>
2023-08-14 05:51:09 +0700

    [1408-12] posts by category

---
+++ blogs/urls.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
+from . import views
+
+urlpatterns = [
+    # path('category/', include('blogs.urls')),
+    path('<int:category_id>/', views.posts_by_category, name='posts_by_category'),
+]
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from blogs.models import Blog
 
 # Create your views here.
+
+
+def posts_by_category(request, category_id):
+    # fetch posts
+    posts = Blog.objects.filter(status="Published", category=category_id)
+    context = {"posts": posts}
+    render(request, "posts_by_category.html", context)
