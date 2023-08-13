5a6d95d4885d4bda1ac4dc2ea06d4b72836324f5
CongSanhLe <congsanhle1999@gmail.com>
2023-08-13 07:49:32 +0700

    display not featured post

--- blog_main/views.py
+++ blog_main/views.py
@@ -6,6 +6,9 @@ from blogs.models import Blog, Category
 def home(request):
     categories = Category.objects.all()
     featured_posts = Blog.objects.filter(
-        is_featured=True).order_by("-update_at")
-    context = {"categories": categories, "featured_posts": featured_posts}
+        is_featured=True, status="Published").order_by("-update_at")
+    not_featured_posts = Blog.objects.filter(
+        is_featured=False, status="Published").order_by("-update_at")
+    context = {"categories": categories, "featured_posts": featured_posts,
+               "not_featured_posts": not_featured_posts}
     return render(request, "home.html", context)
