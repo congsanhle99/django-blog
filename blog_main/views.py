713797c129be1361e6ae8df6fc449784464daa57
CongSanhLe <congsanhle1999@gmail.com>
2023-08-13 07:03:56 +0700

    [1308-11] display category

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
+from blogs.models import Category
+
 
 def home(request):
-    context = {}
+    categories = Category.objects.all()
+    context = {"categories": categories}
     return render(request, "home.html", context)
