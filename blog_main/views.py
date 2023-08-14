b4297182d5d7fc55ca8a920c0f9e8af29c0a2857
CongSanhLe <congsanhle1999@gmail.com>
2023-08-15 06:59:31 +0700

    login n logout func

--- blog_main/views.py
+++ blog_main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from blog_main.forms import RegistrationForm
 from blogs.models import Blog, Category
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
 
 def home(request):
@@ -30,7 +31,23 @@ def register(request):
 
 
 def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+            return redirect("home")
+
     form = AuthenticationForm()
 
     context = {"form": form}
     return render(request, "login.html", context)
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect("home")
