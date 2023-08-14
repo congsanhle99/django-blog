39a25eabc9b3882edc60adb7ad5a7d2ac4d50342
CongSanhLe <congsanhle1999@gmail.com>
2023-08-14 07:22:20 +0700

    DRY inheritance base html

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--5x)!rj&+er1#$#8=#e3lpo%kqx@v1s6f4-6_lll^!7hmkhhsw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
