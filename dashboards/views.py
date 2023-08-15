f7e0f3b9b9a0d73d206e8da8f1903c3c2219e67a
CongSanhLe <congsanhle1999@gmail.com>
2023-08-16 06:25:31 +0700

    [1608-20] manager user dashboard

--- dashboards/views.py
+++ dashboards/views.py
@@ -3,6 +3,7 @@ from blogs.models import Category, Blog
 from django.contrib.auth.decorators import login_required
 from .forms import BlogPostForm, CategoryForm
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 # Create your views here.
 
 
@@ -104,3 +105,9 @@ def delete_post(request, pk):
     post = get_object_or_404(Blog, pk=pk)
     post.delete()
     return redirect('posts')
+
+
+def users(request):
+    users = User.objects.all()
+    context = {"users": users}
+    return render(request, "dashboard/users.html", context)
