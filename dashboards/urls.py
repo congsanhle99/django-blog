398b9dfbea38f21db278145a33d625f0fc83565c
CongSanhLe <congsanhle1999@gmail.com>
2023-08-15 12:25:00 +0700

    add new category

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
 
     path('', views.dashboard, name='dashboard'),
     path('categories/', views.categories, name='categories'),
+    path('categories/add', views.add_category, name='add_category'),
 ]
--- dashboards/views.py
+++ dashboards/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
 from blogs.models import Category, Blog
 from django.contrib.auth.decorators import login_required
+from .forms import CategoryForm
 
 # Create your views here.
 
@@ -17,3 +17,16 @@ def dashboard(request):
 def categories(request):
     context = {}
     return render(request, "dashboard/categories.html", context)
+
+
+def add_category(request):
+    if request.method == "POST":
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('categories')
+
+    form = CategoryForm()
+
+    context = {"form": form}
+    return render(request, "dashboard/add_category.html", context)
